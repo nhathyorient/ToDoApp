31eccd73b9cdf99f4c6c52acb8e392ec99552e46
Duc Dang <duc01226@gmail.com>
2022-06-28 14:19:13 +0700

    Add Example Api Using Jwt Authentication

---
+++ LearningAuthenticationExamples/1_ApiJwtAuthenticationExample/Controllers/WeatherForecastController.cs
@@ -0,0 +1,51 @@
+using ApiJwtAuthenticationExample.Auth.Const;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiJwtAuthenticationExample.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [Authorize(Roles = UserRoles.Admin)]
+        [HttpGet]
+        [Route("for-admin")]
+        public IEnumerable<WeatherForecast> GetForAdmin()
+        {
+            return Enumerable.Range(1, 5)
+                .Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                })
+                .ToArray();
+        }
+    }
+}
